40f91dc9034a5e04acd42062a54bf8fe795a6215
masakorlando <masako@vt.edu>
2025-11-07 14:47:19 -0500

    create masakorlando.java

---
+++ src/prj5/masakorlando.java
@@ -0,0 +1,19 @@
+package prj5;
+// -------------------------------------------------------------------------
+/**
+ *  Write a one-sentence summary of your class here.
+ *  Follow it with additional details about its purpose, what abstraction
+ *  it represents, and how to use it.
+ * 
+ *  @author masak
+ *  @version Nov 7, 2025
+ */
+public class masakorlando
+{
+    //~ Fields ................................................................
+
+    //~ Constructors ..........................................................
+
+    //~Public  Methods ........................................................
+
+}
