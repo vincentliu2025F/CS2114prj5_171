200ecf77e6d9ec8055aba47dab834ca8d7a01d4f
ryanjeronimus <ryanjeronimus@vt.edu>
2025-11-07 14:48:02 -0500

    adding print

--- src/prj5/ryanjeronimus.java
+++ src/prj5/ryanjeronimus.java
@@ -6,5 +6,10 @@ public class ryanjeronimus
     //~ Constructors ..........................................................
 
     //~Public  Methods ........................................................
+    public static void main(String[] args) {
+
+        System.out.println("Glad to be on your team");
+
+    }
 
 }
