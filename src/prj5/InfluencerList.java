5eda875aa8b388acdc6310cb981a2ba7aa462cf5
LeScooot <jeronimus.ryan@gmail.com>
2025-11-17 18:53:23 -0500

    basic InfluencerList done

--- src/prj5/InfluencerList.java
+++ src/prj5/InfluencerList.java
@@ -2,6 +2,14 @@ package prj5;
 
 import java.util.*;
 
+// -------------------------------------------------------------------------
+/**
+ * Contains logic and code for the implementation of a linked-list of
+ * influencers. Uses 3 comparators to sort the list in various ways.
+ * 
+ * @author ryanjeronimus
+ * @version Nov 17, 2025
+ */
 public class InfluencerList
     implements ListInterface<Influencer>
 {
@@ -71,6 +79,28 @@ public class InfluencerList
     @Override
     public void add(int newPosition, Influencer newEntry)
     {
+        if (newPosition < 1 || newPosition > size + 1)
+        {
+            throw new IndexOutOfBoundsException("Illegal position argument");
+        }
+
+        Node<Influencer> newNode = new Node<>(newEntry);
+        if (newPosition == 1)
+        {
+            newNode.setNext(firstNode);
+            firstNode = newNode;
+        }
+        else
+        {
+            Node<Influencer> nodeBefore = firstNode;
+            for (int i = 1; i < newPosition - 1; i++)
+            {
+                nodeBefore = nodeBefore.getNext();
+            }
+            newNode.setNext(nodeBefore.getNext());
+            nodeBefore.setNext(newNode);
+        }
+        size++;
 
     }
 
@@ -78,8 +108,31 @@ public class InfluencerList
     @Override
     public Influencer remove(int givenPosition)
     {
-        // TODO Auto-generated method stub
-        return null;
+        if (givenPosition < 1 || givenPosition > size)
+        {
+            throw new IndexOutOfBoundsException("Illegal position argument");
+        }
+        Influencer currInfluencer;
+        if (givenPosition == 1)
+        {
+            currInfluencer = firstNode.getData();
+            firstNode = firstNode.getNext();
+        }
+        else
+        {
+            Node<Influencer> nodeBefore = firstNode;
+            for (int i = 1; i < givenPosition - 1; i++)
+            {
+                nodeBefore = nodeBefore.getNext();
+            }
+
+            Node<Influencer> remover = nodeBefore.getNext();
+            currInfluencer = remover.getData();
+            nodeBefore.setNext(remover.getNext());
+        }
+
+        size--;
+        return currInfluencer;
     }
 
 
@@ -95,31 +148,64 @@ public class InfluencerList
     @Override
     public Influencer replace(int givenPosition, Influencer newEntry)
     {
-        // TODO Auto-generated method stub
-        return null;
+        if (givenPosition < 1 || givenPosition > size)
+        {
+            throw new IndexOutOfBoundsException("Illegal position argument");
+        }
+        Node<Influencer> currentNode = firstNode;
+        for (int i = 1; i < givenPosition; i++)
+        {
+            currentNode = currentNode.getNext();
+        }
+        Influencer oldData = currentNode.getData();
+        currentNode.setData(newEntry);
+        return oldData;
     }
 
 
     @Override
     public Influencer getEntry(int givenPosition)
     {
-        // TODO Auto-generated method stub
-        return null;
+        if (givenPosition < 1 || givenPosition > size)
+        {
+            throw new IndexOutOfBoundsException("Illegal position argument");
+        }
+        Node<Influencer> currentNode = firstNode;
+        for (int i = 1; i < givenPosition; i++)
+        {
+            currentNode = currentNode.getNext();
+        }
+        return currentNode.getData();
     }
 
 
     @Override
     public Object[] toArray()
     {
-        // TODO Auto-generated method stub
-        return null;
+        Object[] influencerArray = new Object[size];
+        Node<Influencer> currNode = firstNode;
+        for (int i = 0; i < size; i++)
+        {
+            influencerArray[i] = currNode.getData();
+            currNode = currNode.getNext();
+        }
+        return influencerArray;
     }
 
 
     @Override
     public boolean contains(Influencer anEntry)
     {
-        // TODO Auto-generated method stub
+        Node<Influencer> currNode = firstNode;
+
+        for (int i = 0; i < this.size; i++)
+        {
+            if (currNode.getData().equals(anEntry))
+            {
+                return true;
+            }
+            currNode = currNode.getNext();
+        }
         return false;
     }
 
