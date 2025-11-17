e2ae69b0abb6ebb090bf40b6050036d9c3d459b0
LeScooot <jeronimus.ryan@gmail.com>
2025-11-17 18:45:32 -0500

    adding CS2114 list interface

--- src/prj5/InfluencerList.java
+++ src/prj5/InfluencerList.java
@@ -3,7 +3,7 @@ package prj5;
 import java.util.*;
 
 public class InfluencerList
-    implements List<Influencer>
+    implements ListInterface<Influencer>
 {
     private class Node<T>
     {
@@ -47,8 +47,8 @@ public class InfluencerList
         }
     }
 
-    Node<Influencer> firstNode;
-    int size;
+    private Node<Influencer> firstNode;
+    private int size;
 
     public InfluencerList()
     {
@@ -56,48 +56,52 @@ public class InfluencerList
         size = 0;
     }
 
-    @Override
-    public int size()
-    {
 
-        return this.size;
+    @Override
+    public void add(Influencer newEntry)
+    {
+        Node<Influencer> newNode = new Node<Influencer>(newEntry);
+        newNode.setNext(firstNode);
+        firstNode = newNode;
+        this.size++;
+
     }
 
 
     @Override
-    public boolean isEmpty()
+    public void add(int newPosition, Influencer newEntry)
     {
-        return this.size == 0;
+
     }
 
 
     @Override
-    public boolean contains(Object object)
+    public Influencer remove(int givenPosition)
     {
-        if (object == null)
-        {
-            return false;
-        }
-        if (!(object instanceof Influencer))
-        {
-            return false;
-        }
-        Influencer influencerObject = (Influencer)object;
-        Node<Influencer> currNode = firstNode;
-        for (int i = 0; i < size; i++)
-        {
-            if (currNode.getData().equals(influencerObject))
-            {
-                return true;
-            }
-            currNode = currNode.getNext();
-        }
-        return false;
+        // TODO Auto-generated method stub
+        return null;
     }
 
 
     @Override
-    public Iterator<Influencer> iterator()
+    public void clear()
+    {
+        firstNode = null;
+        this.size = 0;
+
+    }
+
+
+    @Override
+    public Influencer replace(int givenPosition, Influencer newEntry)
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+
+    @Override
+    public Influencer getEntry(int givenPosition)
     {
         // TODO Auto-generated method stub
         return null;
@@ -113,59 +117,7 @@ public class InfluencerList
 
 
     @Override
-    public <T> T[] toArray(T[] a)
-    {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-
-    @Override
-    public boolean add(Influencer newInfluencer)
-    {
-        Node<Influencer> newNode = new Node<Influencer>(newInfluencer);
-        newNode.setNext(firstNode);
-        firstNode = newNode;
-        this.size++;
-        return true;
-    }
-
-
-    @Override
-    public boolean remove(Object object)
-    {
-        if (object == null)
-        {
-            return false;
-        }
-        if (!(object instanceof Influencer))
-        {
-            return false;
-        }
-        Influencer influencerObject = (Influencer)object;
-        if (firstNode.getData() == influencerObject)
-        {
-            firstNode = firstNode.getNext();
-            this.size--;
-            return true;
-        }
-
-        Node<Influencer> currNode = firstNode;
-        for (int i = 0; i < size; i++)
-        {
-            if (currNode.getNext().equals(influencerObject))
-            {
-                currNode.setNext(currNode.getNext().getNext());
-                this.size--;
-                return true;
-            }
-        }
-        return false;
-    }
-
-
-    @Override
-    public boolean containsAll(Collection<?> c)
+    public boolean contains(Influencer anEntry)
     {
         // TODO Auto-generated method stub
         return false;
@@ -173,115 +125,16 @@ public class InfluencerList
 
 
     @Override
-    public boolean addAll(Collection<? extends Influencer> c)
+    public int getLength()
     {
-        // TODO Auto-generated method stub
-        return false;
+        return this.size;
     }
 
 
     @Override
-    public boolean addAll(int index, Collection<? extends Influencer> c)
+    public boolean isEmpty()
     {
-        // TODO Auto-generated method stub
-        return false;
-    }
-
-
-    @Override
-    public boolean removeAll(Collection<?> c)
-    {
-        // TODO Auto-generated method stub
-        return false;
-    }
-
-
-    @Override
-    public boolean retainAll(Collection<?> c)
-    {
-        // TODO Auto-generated method stub
-        return false;
-    }
-
-
-    @Override
-    public void clear()
-    {
-        firstNode.setNext(null);
-        this.size = 0;
-
-    }
-
-
-    @Override
-    public Influencer get(int index)
-    {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-
-    @Override
-    public Influencer set(int index, Influencer element)
-    {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-
-    @Override
-    public void add(int index, Influencer element)
-    {
-        // TODO Auto-generated method stub
-
-    }
-
-
-    @Override
-    public Influencer remove(int index)
-    {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-
-    @Override
-    public int indexOf(Object o)
-    {
-        // TODO Auto-generated method stub
-        return 0;
-    }
-
-
-    @Override
-    public int lastIndexOf(Object o)
-    {
-        // TODO Auto-generated method stub
-        return 0;
-    }
-
-
-    @Override
-    public ListIterator<Influencer> listIterator()
-    {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-
-    @Override
-    public ListIterator<Influencer> listIterator(int index)
-    {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-
-    @Override
-    public List<Influencer> subList(int fromIndex, int toIndex)
-    {
-        // TODO Auto-generated method stub
-        return null;
+        return this.size == 0;
     }
 
 }
