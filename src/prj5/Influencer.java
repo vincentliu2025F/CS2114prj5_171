9647a4993623c07c56d859564bbf260c6ab6e553
nabiilf <nabiilf@vt.edu>
2025-11-18 23:32:48 -0500

    added influencer code

--- src/prj5/Influencer.java
+++ src/prj5/Influencer.java
@@ -1,10 +1,89 @@
 package prj5;
-public class Influencer
-{
-    //~ Fields ................................................................
 
-    //~ Constructors ..........................................................
+public class Influencer {
 
-    //~Public  Methods ........................................................
+    // Fields
+    private String username;
+    private String channelName;
+    private String country;
+    private String topics;
+    private int likes;
+    private int posts;
+    private int followers;
+    private int comments;
+    private int views;
 
+    // Getters
+    public String getUsername() {
+        return username;
+    }
+
+    public String getChannelName() {
+        return channelName;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public String getTopics() {
+        return topics;
+    }
+
+    public int getLikes() {
+        return likes;
+    }
+
+    public int getPosts() {
+        return posts;
+    }
+
+    public int getFollowers() {
+        return followers;
+    }
+
+    public int getComments() {
+        return comments;
+    }
+
+    public int getViews() {
+        return views;
+    }
+
+    // Setters
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setChannelName(String channelName) {
+        this.channelName = channelName;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public void setTopics(String topics) {
+        this.topics = topics;
+    }
+
+    public void setLikes(int likes) {
+        this.likes = likes;
+    }
+
+    public void setPosts(int posts) {
+        this.posts = posts;
+    }
+
+    public void setFollowers(int followers) {
+        this.followers = followers;
+    }
+
+    public void setComments(int comments) {
+        this.comments = comments;
+    }
+
+    public void setViews(int views) {
+        this.views = views;
+    }
 }
